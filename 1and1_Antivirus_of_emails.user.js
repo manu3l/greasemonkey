6476438ed33d785cd16c4a07d59fd9074e8c1656
manu3l <6281205+manu3l@users.noreply.github.com>
2017-10-24 10:12:39 +0200

    Update 1and1_Antivirus_of_emails.user.js
    New version 3, no more need for user action.
    The script:
    - open all antivirus link in tabs
    - disable the antivirus with an click emulate
    - close all tabs

--- 1and1_Antivirus_of_emails.user.js
+++ 1and1_Antivirus_of_emails.user.js
@@ -1,38 +1,71 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name      1and1 Antivirus of emails
-// @namespace http://freeshell.de/~manu67/pub/
-// @description Help to disable antivirus of mail on the 1&1 Customer Area
+// @namespace https://github.com/manu3l/greasemonkey
+// @description Disable antivirus of mail on the 1&1 Customer Area
 // @include     https://clients.1and1.fr/CenterCommunication*
-// @version    2.0
+// @include     https://clients.1and1.fr/email-virus-protection/mailbox/*
+// @version     3.0
+// @icon        https://contact.1and1.fr/img/components/header/favicon.ico
 // @run-at      document-end
 // ==/UserScript==/
 
-var divAntivirus = document.getElementById('email-overview');
-divAntivirus.insertAdjacentHTML('beforebegin', '<div><button class="button-a1" id="antivirus" type="button">antivirus</button></div>');
-//--- Activate the newly added button.
-document.getElementById('antivirus').addEventListener('click', ButtonClickAction, false);
-
+/* no more used
 function InsertNodeAfter(newElement, targetElement) {
   var parent = targetElement.parentNode;
   if (parent.lastChild == targetElement)
   parent.appendChild(newElement);
-   else
-  parent.insertBefore(newElement, targetElement.nextSibling);
+  //else
+  //parent.insertBefore(newElement, targetElement.nextSibling);
 }
 
-function addCustomSearchResult(jNode) {
-  var links = document.querySelectorAll('a[href^="/email-quota-upgrade"]');
+function addCustomSearchResult(WhereToAdd) {
+  var WhereToAddUrl = document.querySelectorAll('a[href*=\'' + WhereToAdd + '\'].standard');
+  var links = document.querySelectorAll('td.address > a[href*=\'mailbox\'].email-address');
   for (var J = links.length - 1; J >= 0; --J) {
     var thisLink = links[J];
+    WhereToAdd = WhereToAddUrl[J];
     var newElement = document.createElement('span');
-    newURL = thisLink.href.replace('https://clients.1and1.fr/email-quota-upgrade?id=', 'https://clients.1and1.fr/Email_SingleVirusProtection?id=');
+    newURL = thisLink.href.replace(RegExp('https://clients.1and1.fr\\/email-account-details\\/mailbox\\/(.*)?__lf=MsexchangeCreateFlow'), 'https://clients.1and1.fr/email-virus-protection/mailbox/$1?__lf=email_change_type');
     newElement.innerHTML = '<a class="standard anker-a1" href="' + newURL + '"> | AV</a>';
-    InsertNodeAfter(newElement, thisLink);
+    InsertNodeAfter(newElement, WhereToAdd);
   }
 }
 
 function ButtonClickAction(zEvent) {
-  /*--- For our dummy action, we'll just add a line of text to the top of the screen.
-    */
-  addCustomSearchResult();
+  addCustomSearchResult('email-quota-upgrade');
 }
+*/
+
+/* open url in tabs for antivirus switch */
+function openAntiVirusTab() {
+  var links = document.querySelectorAll('td.address > a[href*=\'mailbox\'].email-address');
+  var antiviruslink = document.querySelectorAll('td.column-4 > a, td.column-4 > span');
+  for (var J = links.length - 1; J >= 0; --J) {
+    var thisLink = links[J];
+    var antivirusornot = antiviruslink[J];
+    if (antivirusornot.className == 'markup-before icon-font checked') {
+      newURL = thisLink.href.replace(RegExp('https://clients.1and1.fr\\/email-account-details\\/mailbox\\/(.*)?__lf=MsexchangeCreateFlow'), 'https://clients.1and1.fr/email-virus-protection/mailbox/$1?__lf=email_change_type');
+      window.open(newURL);
+    }
+  }
+}
+
+/* open from the opened tab, switch the button and close the tabs*/
+function clickAV() {
+  var avButton = document.getElementById('email-edit-virus-activate');
+  if (avButton && avButton.value == 'true') {
+    avButton.click();
+    setTimeout(function () {
+      window.close();
+    }, 2000);
+  }
+}
+var divAntivirus = document.getElementById('email-overview');
+if (divAntivirus) {
+  divAntivirus.insertAdjacentHTML('beforebegin', //'<button class="button-a1" id="antivirus" type="button">antivirus</button> ' +
+  '<button class="button-a1" id="Allantivirus" type="button" title="Désactive tous les antivirus activé sur l\'affichage courant">Antivirus</button>');
+  /* no more used
+    document.getElementById('antivirus').addEventListener('click', ButtonClickAction, false);*/
+  document.getElementById('Allantivirus').addEventListener('click', openAntiVirusTab, false);
+}
+clickAV();
