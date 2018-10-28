55ed72328eab81aeae1a46bca9af3978e91fe716
manu3l <6281205+manu3l@users.noreply.github.com>
2018-10-28 22:51:32 +0100

    remove old code

--- 1and1_Antivirus_of_emails.user.js
+++ 1and1_Antivirus_of_emails.user.js
@@ -9,33 +9,6 @@
 // @run-at      document-end
 // ==/UserScript==/
 
-/* no more used
-function InsertNodeAfter(newElement, targetElement) {
-  var parent = targetElement.parentNode;
-  if (parent.lastChild == targetElement)
-  parent.appendChild(newElement);
-  //else
-  //parent.insertBefore(newElement, targetElement.nextSibling);
-}
-
-function addCustomSearchResult(WhereToAdd) {
-  var WhereToAddUrl = document.querySelectorAll('a[href*=\'' + WhereToAdd + '\'].standard');
-  var links = document.querySelectorAll('td.address > a[href*=\'mailbox\'].email-address');
-  for (var J = links.length - 1; J >= 0; --J) {
-    var thisLink = links[J];
-    WhereToAdd = WhereToAddUrl[J];
-    var newElement = document.createElement('span');
-    newURL = thisLink.href.replace(RegExp('https://clients.1and1.fr\\/email-account-details\\/mailbox\\/(.*)?__lf=MsexchangeCreateFlow'), 'https://clients.1and1.fr/email-virus-protection/mailbox/$1?__lf=email_change_type');
-    newElement.innerHTML = '<a class="standard anker-a1" href="' + newURL + '"> | AV</a>';
-    InsertNodeAfter(newElement, WhereToAdd);
-  }
-}
-
-function ButtonClickAction(zEvent) {
-  addCustomSearchResult('email-quota-upgrade');
-}
-*/
-
 /* open url in tabs for antivirus switch */
 function openAntiVirusTab() {
   var links = document.querySelectorAll('td.address > a[href*=\'mailbox\'].email-address');
